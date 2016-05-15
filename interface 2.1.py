e0ca52d6c436a4baabf033d566cdd6c27c3cd035
nathanlederman <nathan.lederman1996@gmail.com>
2016-05-15 12:20:11 -0300

    versão 2.3
    não  ha mais botões. a funcao bt_onclick pega o No da maleta, abre uma
    imagem de maleta e “desliga” o label relacionado ao valor da mesma

--- interface 2.1.py
+++ interface 2.1.py
@@ -9,15 +9,14 @@
 
 from tkinter import *
 
+#imagem == maleta.jpg
 
-def bt_click():
-    print("A maleta vale",  "reais")
 def bt_onclick():
+    #open imagem maleta
+    # turn off label valor X
+
     print(ed.get())
-   
-    
-#def proposta():
-#    print("A proposta do banqueiro foi aceita, voce ganhou", "reais")
+
 
 
 janela = Tk()
@@ -28,169 +27,169 @@ lb27= Label(janela,width = 25,fg="black",bg="gold",text= "Digite a maleta a ser 
 lb27.place(x= 300, y = 390)
 ed= Entry(janela,width = 25,fg="black",bg="gold")
 ed.place(x= 300, y =420 )
-bt27 = Button(janela,width= 25, text = "OK",fg="red", command = bt_onclick )
+bt27 = Button(janela,width= 25, text = "OK",fg="black", command = bt_onclick )
 bt27.place(x= 300, y =450)
 
 
-## Botoes
-bt1= Button(janela,width = 15, text = "Maleta 1",fg= "gold",command=bt_onclick)
-bt1.place(x = 300, y =0)
+## Maletas
+lb1= Label(janela,width = 15, text = "Maleta 1",bg= "black",fg= "gold")
+lb1.place(x = 300, y =0)
 
-bt2= Button(janela,width = 15, text = "Maleta 2" ,fg="gold",command=bt_onclick)
-bt2.place(x = 300, y =30)
+lb2= Label(janela,width = 15, text = "Maleta 2" ,bg= "black",fg= "gold")
+lb2.place(x = 300, y =30)
 
-bt3= Button(janela,width = 15, text = "Maleta 3" ,fg="gold",command=bt_onclick)
-bt3.place(x = 300, y =60)
+lb3= Label(janela,width = 15, text = "Maleta 3" ,bg= "black",fg= "gold")
+lb3.place(x = 300, y =60)
 
-bt4= Button(janela,width = 15, text = "Maleta 4" ,fg="gold",command=bt_onclick)
-bt4.place(x = 300, y =90)
+lb4= Label(janela,width = 15, text = "Maleta 4" ,bg= "black",fg= "gold")
+lb4.place(x = 300, y =90)
 
-bt5= Button(janela,width = 15, text = "Maleta 5" ,fg="gold",command=bt_onclick)
-bt5.place(x = 300, y =120)
+lb5= Label(janela,width = 15, text = "Maleta 5" ,bg= "black",fg= "gold")
+lb5.place(x = 300, y =120)
 
-bt6= Button(janela,width = 15, text = "Maleta 6" ,fg="gold",command=bt_onclick)
-bt6.place(x = 300, y =150)
+lb6= Label(janela,width = 15, text = "Maleta 6" ,bg= "black",fg= "gold")
+lb6.place(x = 300, y =150)
 
-bt7= Button(janela,width = 15, text = "Maleta 7" ,fg="gold",command=bt_onclick)
-bt7.place(x = 300, y =180)
+lb7= Label(janela,width = 15, text = "Maleta 7" ,bg= "black",fg= "gold")
+lb7.place(x = 300, y =180)
 
-bt8= Button(janela,width = 15, text = "Maleta 8" ,fg="gold",command=bt_onclick)
-bt8.place(x = 300, y =210)
+lb8= Label(janela,width = 15, text = "Maleta 8" ,bg= "black",fg= "gold")
+lb8.place(x = 300, y =210)
 
-bt9= Button(janela,width = 15, text = "Maleta 9" ,fg="gold",command=bt_onclick)
-bt9.place(x = 300, y =240)
+lb9= Label(janela,width = 15, text = "Maleta 9" ,bg= "black",fg= "gold")
+lb9.place(x = 300, y =240)
 
-bt10= Button(janela,width = 15, text = "Maleta 10" ,fg="gold",command=bt_onclick)
-bt10.place(x = 300, y =270)
+lb10= Label(janela,width = 15, text = "Maleta 10" ,bg= "black",fg= "gold")
+lb10.place(x = 300, y =270)
 
-bt11= Button(janela,width = 15, text = "Maleta 11" ,fg="gold",command=bt_onclick)
-bt11.place(x = 300, y =300)
+lb11= Label(janela,width = 15, text = "Maleta 11" ,bg= "black",fg= "gold")
+lb11.place(x = 300, y =300)
 
-bt12= Button(janela,width = 15, text = "Maleta 12" ,fg="gold",command=bt_onclick)
-bt12.place(x = 300, y =330)
+lb12= Label(janela,width = 15, text = "Maleta 12" ,bg= "black",fg= "gold")
+lb12.place(x = 300, y =330)
 
-bt13= Button(janela,width = 15, text = "Maleta 13" ,fg="gold",command=bt_onclick)
-bt13.place(x = 300, y =360)
+lb13= Label(janela,width = 15, text = "Maleta 13" ,bg= "black",fg= "gold")
+lb13.place(x = 300, y =360)
 
-bt14= Button(janela,width = 15, text = "Maleta 14" ,fg="gold",command=bt_onclick)
-bt14.place(x = 450, y =0)
+lb14= Label(janela,width = 15, text = "Maleta 14" ,bg= "black",fg= "gold")
+lb14.place(x = 450, y =0)
 
-bt15= Button(janela,width = 15, text = "Maleta 15" ,fg="gold",command=bt_onclick)
-bt15.place(x = 450, y =30)
+lb15= Label(janela,width = 15, text = "Maleta 15" ,bg= "black",fg= "gold")
+lb15.place(x = 450, y =30)
 
-bt16= Button(janela,width = 15, text = "Maleta 16" ,fg="gold",command=bt_onclick)
-bt16.place(x = 450, y =60)
+lb16= Label(janela,width = 15, text = "Maleta 16" ,bg= "black",fg= "gold")
+lb16.place(x = 450, y =60)
 
-bt17= Button(janela,width = 15, text = "Maleta 17" ,fg="gold",command=bt_onclick)
-bt17.place(x = 450, y =90)
+lb17= Label(janela,width = 15, text = "Maleta 17" ,bg= "black",fg= "gold")
+lb17.place(x = 450, y =90)
 
-bt18= Button(janela,width = 15, text = "Maleta 18" ,fg="gold",command=bt_onclick)
-bt18.place(x = 450, y =120)
+lb18= Label(janela,width = 15, text = "Maleta 18" ,bg= "black",fg= "gold")
+lb18.place(x = 450, y =120)
 
-bt19= Button(janela,width = 15, text = "Maleta 19" ,fg="gold",command=bt_onclick)
-bt19.place(x = 450, y =150)
+lb19= Label(janela,width = 15, text = "Maleta 19" ,bg= "black",fg= "gold")
+lb19.place(x = 450, y =150)
 
-bt20= Button(janela,width = 15, text = "Maleta 20" ,fg="gold",command=bt_onclick)
-bt20.place(x = 450, y =180)
+lb20= Label(janela,width = 15, text = "Maleta 20" ,bg= "black",fg= "gold")
+lb20.place(x = 450, y =180)
 
-bt21= Button(janela,width = 15, text = "Maleta 21" ,fg="gold",command=bt_onclick)
-bt21.place(x = 450, y =210)
+lb21= Label(janela,width = 15, text = "Maleta 21" ,bg= "black",fg= "gold")
+lb21.place(x = 450, y =210)
 
-bt22= Button(janela,width = 15, text = "Maleta 22" ,fg="gold",command=bt_onclick)
-bt22.place(x = 450, y =240)
+lb22= Label(janela,width = 15, text = "Maleta 22" ,bg= "black",fg= "gold")
+lb22.place(x = 450, y =240)
 
-bt23= Button(janela,width = 15, text = "Maleta 23" ,fg="gold",command=bt_onclick)
-bt23.place(x = 450, y =270)
+lb23= Label(janela,width = 15, text = "Maleta 23" ,bg= "black",fg= "gold")
+lb23.place(x = 450, y =270)
 
-bt24= Button(janela,width = 15, text = "Maleta 24" ,fg="gold",command=bt_onclick)
-bt24.place(x = 450, y =300)
+lb24= Label(janela,width = 15, text = "Maleta 24" ,bg= "black",fg= "gold")
+lb24.place(x = 450, y =300)
 
-bt25= Button(janela,width = 15, text = "Maleta 25" ,fg="gold",command=bt_onclick)
-bt25.place(x = 450, y =330)
+lb25= Label(janela,width = 15, text = "Maleta 25" ,bg= "black",fg= "gold")
+lb25.place(x = 450, y =330)
 
-bt26= Button(janela,width = 15, text = "Maleta 26" ,fg="gold",command=bt_onclick)
-bt26.place(x = 450, y =360)
+lb26= Label(janela,width = 15, text = "Maleta 26" ,bg= "black",fg= "gold")
+lb26.place(x = 450, y =360)
 
 
 
 ## Valores
-lb1= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 0.5")
-lb1.place(x = 1, y =0)
+lb28= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 0.5")
+lb28.place(x = 1, y =0)
 
-lb2= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1")
-lb2.place(x = 1, y =30)
+lb29= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1")
+lb29.place(x = 1, y =30)
 
-lb3= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 5")
-lb3.place(x = 1, y =60)
+lb30= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 5")
+lb30.place(x = 1, y =60)
 
-lb4= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 10")
-lb4.place(x = 1, y =90)
+lb31= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 10")
+lb31.place(x = 1, y =90)
 
-lb5= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 25")
-lb5.place(x = 1, y =120)
+lb32= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 25")
+lb32.place(x = 1, y =120)
 
-lb6= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 50")
-lb6.place(x = 1, y =150)
+lb33= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 50")
+lb33.place(x = 1, y =150)
 
-lb7= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 75")
-lb7.place(x = 1, y =180)
+lb34= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 75")
+lb34.place(x = 1, y =180)
 
-lb8= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 100")
-lb8.place(x = 1, y =210)
+lb35= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 100")
+lb35.place(x = 1, y =210)
 
-lb9= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 200")
-lb9.place(x = 1, y =240)
+lb36= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 200")
+lb36.place(x = 1, y =240)
 
-lb10= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 300")
-lb10.place(x = 1, y =270)
+lb37= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 300")
+lb37.place(x = 1, y =270)
 
-lb11= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 400")
-lb11.place(x = 1, y =300)
+lb38= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 400")
+lb38.place(x = 1, y =300)
 
-lb12= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 500")
-lb12.place(x = 1, y =330)
+lb39= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 500")
+lb39.place(x = 1, y =330)
 
-lb13= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 750")
-lb13.place(x = 1, y =360)
+lb40= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 750")
+lb40.place(x = 1, y =360)
 
-lb14= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1.000")
-lb14.place(x = 150, y =0)
+lb41= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1.000")
+lb41.place(x = 150, y =0)
 
-lb15= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 5.000")
-lb15.place(x = 150, y =30)
+lb42= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 5.000")
+lb42.place(x = 150, y =30)
 
-lb16= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 10.000")
-lb16.place(x = 150, y =60)
+lb43= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 10.000")
+lb43.place(x = 150, y =60)
 
-lb17= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 25.000")
-lb17.place(x = 150, y =90)
+lb44= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 25.000")
+lb44.place(x = 150, y =90)
 
-lb18= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 50.000")
-lb18.place(x = 150, y =120)
+lb45= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 50.000")
+lb45.place(x = 150, y =120)
 
-lb19= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 75.000")
-lb19.place(x = 150, y =150)
+lb46= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 75.000")
+lb46.place(x = 150, y =150)
 
-lb20= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 100.000")
-lb20.place(x = 150, y =180)
+lb47= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 100.000")
+lb47.place(x = 150, y =180)
 
-lb21= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 200.000")
-lb21.place(x = 150, y =210)
+lb48= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 200.000")
+lb48.place(x = 150, y =210)
 
-lb22= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 300.000")
-lb22.place(x = 150, y =240)
+lb49= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 300.000")
+lb49.place(x = 150, y =240)
 
-lb23= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 400.000")
-lb23.place(x = 150, y =270)
+lb50= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 400.000")
+lb50.place(x = 150, y =270)
 
-lb24= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 500.000")
-lb24.place(x = 150, y =300)
+lb51= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 500.000")
+lb51.place(x = 150, y =300)
 
-lb25= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 750.000")
-lb25.place(x = 150, y =330)
+lb52= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 750.000")
+lb52.place(x = 150, y =330)
 
-lb26= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1.000.000")
-lb26.place(x = 150, y =360)
+lb53= Label(janela,width = 15,fg="black",bg="gold", text = "R$ 1.000.000")
+lb53.place(x = 150, y =360)
 
 
 
